fc431fedf3e0575731f3ad1f7fa42ccedb9190a7
ddyy <wntnftk79@gmail.com>
2023-11-23 08:06:25 +0900

    할일종료표시

--- src/App.js
+++ src/App.js
@@ -22,9 +22,23 @@ const TodoItemInputField = (props) => {
     <Button variant="outlined" onClick={onSubmit}>Submit</Button>  
   </div>);
 };
+
+const TodoItem = (props) => {
+  const style = props.todoItem.isFinished ? { textDecoration: 'line-through' } : {}; //할일종료표시 > Finished 를 처리해줘야한다. 어디서?
+  return (<li>
+    {/* <span>{props.todoItem.todoItemContent}</span> */}
+    <span
+      style={style}
+      onClick={() => props.onTodoItemClick(props.todoItem)}
+    >
+      {props.todoItem.todoItemContent}
+    </span>
+  </li>);
+}
 const TodoItemList = (props) => {
   const todoList = props.todoItemList.map((todoItem, index) => {
-    return <li key={index}>{todoItem.todoItemContent}</li>;
+    // return <li key={index}>{todoItem.todoItemContent}</li>;
+    return <TodoItem key={todoItem.id} todoItem={todoItem} onTodoItemClick={props.onTodoItemClick}/>; //여기서
   });
   return (<div>
     <ul>{todoList}</ul>
@@ -43,12 +57,27 @@ function App() {
       isFinished: false,
     }]);
   };
+
+  //실제 바뀌는건 여기서 일어남
+  const onTodoItemClick = (clickedTodoItem) => {
+    setTodoItemList(todoItemList.map((todoItem) => {
+      if (clickedTodoItem.id === todoItem.id) {
+        return {
+          id: clickedTodoItem.id,
+          todoItemContent: clickedTodoItem.todoItemContent,
+          isFinished: !clickedTodoItem.isFinished,
+        };
+      } else {
+        return todoItem;
+      }
+    }))
+  }
   return (
     <div className="App">
       {/* <TodoItemInputField/> */}
       <TodoItemInputField onSubmit={onSubmit} />
       {/* <TodoItemList todoItemList={[]} /> */}
-      <TodoItemList todoItemList={todoItemList} />
+      <TodoItemList todoItemList={todoItemList} onTodoItemClick={onTodoItemClick}/>
     </div>
   );
 }
