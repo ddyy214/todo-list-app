4cc3bc64c05e0ea3b23a0a34fdc00b5e2f98b302
ddyy <wntnftk79@gmail.com>
2023-11-23 17:31:01 +0900

    Integrate Firebase

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@ import './App.css';
 import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA09hT0e-RuFbFefDsZoVb3idF5J7g_2a8",
+  authDomain: "todo-list-app-3554d.firebaseapp.com",
+  projectId: "todo-list-app-3554d",
+  storageBucket: "todo-list-app-3554d.appspot.com",
+  messagingSenderId: "5145913718",
+  appId: "1:5145913718:web:15515622d2cdac1fca7257",
+  measurementId: "G-JT6H2KWP74"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const TodoItemInputField = (props) => {
   const [input, setInput] = useState("");
   const onSubmit = () => {
