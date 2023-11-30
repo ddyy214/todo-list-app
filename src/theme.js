053b8fb9d726861bc3a381793eee3a5bf37357ef
ddyy <wntnftk79@gmail.com>
2023-11-30 21:00:29 +0900

    theme

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+          main: '#6b7171',
+        },
+        secondary: {
+          main: '#ead6d6', 
+        },
+    
+      },
+    });
+
+export default theme;
